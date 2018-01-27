b24ad4d77590360a26deef28d5c9f22ccaa54d2d
Dominik Hirsch <D.hirsch98@gmx.net>
2018-01-27 01:07:44 +0100

    Tileset

--- Assets/TileFactory.cs
+++ Assets/TileFactory.cs
@@ -10,11 +10,18 @@ public class TileFactory : MonoBehaviour {
 public int tilecount;
 float width;
 float length;
-public GameObject Tile;
-public List<GameObject> Tiles;
+public GameObject Background;
+public GameObject Stonewall;
+public GameObject Water;
+public GameObject Thorntendrils;
+public GameObject DeepA;
+public GameObject DeepB;
+public GameObject DeepC;
+    
 TileTypes[,] tilemap = new TileTypes[,] {
-        {TileTypes.Background, TileTypes.Stonewall },
-        { TileTypes.Stonewall, TileTypes.Background }
+        {TileTypes.Background, TileTypes.Background , TileTypes.Background },
+        { TileTypes.Water, TileTypes.Background, TileTypes.Water },
+        { TileTypes.Background, TileTypes.Background, TileTypes.Background }
 };
 
 // Use this for initialization
@@ -29,38 +36,38 @@ void Update () {
 
 public IEnumerator createsTiles()
 {
-        for(int i=0; i<tilemap.Rank; i++)
+        for(int i=0; i<tilemap.GetLength(0); i++)
         {
-                for(int j=0; j<tilemap.GetLength(i); j++)
+                for(int j=0; j<tilemap.GetLength(1); j++)
                 {
                         switch (tilemap[i,j]) {
                         case (TileTypes.Background):
                                 Debug.Log ("create Background at " + i + ", " + j);
-                                Instantiate (Tile, new Vector2 (-i, -j), Quaternion.identity);
+                                Instantiate (Background, new Vector2 (-i, -j), Quaternion.identity);
                                 break;
                         case (TileTypes.Stonewall):
                                 Debug.Log ("create Background at " + i + ", " + j);
-                                Instantiate (Tile, new Vector2 (-i, -j), Quaternion.identity);
+                                Instantiate (Stonewall, new Vector2 (-i, -j), Quaternion.identity);
                                 break;
                         case (TileTypes.Water):
                                 Debug.Log ("create Background at " + i + ", " + j);
-                                Instantiate (Tile, new Vector2 (-i, -j), Quaternion.identity);
+                                Instantiate (Water, new Vector2 (-i, -j), Quaternion.identity);
                                 break;
                         case (TileTypes.Thorntendrils):
                                 Debug.Log ("create Background at " + i + ", " + j);
-                                Instantiate (Tile, new Vector2 (-i, -j), Quaternion.identity);
+                                Instantiate (Thorntendrils, new Vector2 (-i, -j), Quaternion.identity);
                                 break;
                         case (TileTypes.Deep1):
                                 Debug.Log ("create Background at " + i + ", " + j);
-                                Instantiate (Tile, new Vector2 (-i, -j), Quaternion.identity);
+                                Instantiate (DeepA, new Vector2 (-i, -j), Quaternion.identity);
                                 break;
                         case (TileTypes.Deep2):
                                 Debug.Log ("create Background at " + i + ", " + j);
-                                Instantiate (Tile, new Vector2 (-i, -j), Quaternion.identity);
+                                Instantiate (DeepB, new Vector2 (-i, -j), Quaternion.identity);
                                 break;
                         case (TileTypes.Deep3):
                                 Debug.Log ("create Background at " + i + ", " + j);
-                                Instantiate (Tile, new Vector2 (-i, -j), Quaternion.identity);
+                                Instantiate (DeepC, new Vector2 (-i, -j), Quaternion.identity);
                                 break;
                         default:
                                 break;
