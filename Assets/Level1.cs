c90fccf62e5451dab09dd19e6037eeff5ff2f167
Danny Martini <danny@wunderflats.com>
2018-01-28 14:34:27 +0100

    lvl up

--- Assets/Level1.cs
+++ Assets/Level1.cs
@@ -19,13 +19,13 @@ static public TileTypes[,] tilemap = new TileTypes[,] {
    {TileTypes.DeepB_90, TileTypes.DeepB_0, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Water, TileTypes.Background} ,
  {TileTypes.DeepB_180, TileTypes.DeepB_270, TileTypes.Background, TileTypes.Water, TileTypes.Background, TileTypes.Water, TileTypes.Background} ,
  {TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Water, TileTypes.Background, TileTypes.Background, TileTypes.Background} ,
- {TileTypes.Background, TileTypes.Water, TileTypes.Water, TileTypes.Water, TileTypes.Water, TileTypes.Water, TileTypes.Water} ,
- {TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Water, TileTypes.Water} ,
- {TileTypes.Water, TileTypes.Water, TileTypes.Background, TileTypes.Stonewall, TileTypes.Background, TileTypes.Background, TileTypes.Water} ,
- {TileTypes.Water, TileTypes.Water, TileTypes.Background, TileTypes.Stonewall, TileTypes.Stonewall, TileTypes.Background, TileTypes.Background} ,
- {TileTypes.Thorntendrils, TileTypes.Thorntendrils, TileTypes.Thorntendrils, TileTypes.Stonewall, TileTypes.Background, TileTypes.Background, TileTypes.Stonewall} ,
- {TileTypes.Background, TileTypes.Background, TileTypes.Thorntendrils, TileTypes.Background, TileTypes.Background, TileTypes.DeepC_270, TileTypes.Stonewall} ,
- {TileTypes.Background, TileTypes.Stonewall, TileTypes.Thorntendrils, TileTypes.Background, TileTypes.DeepC_180, TileTypes.DeepA_0, TileTypes.DeepC_0} ,
+ {TileTypes.Background, TileTypes.Water, TileTypes.Water, TileTypes.Water, TileTypes.Thorntendrils, TileTypes.Water, TileTypes.Water} ,
+ {TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background} ,
+ {TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Stonewall, TileTypes.Background, TileTypes.Background, TileTypes.Background} ,
+ {TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Stonewall, TileTypes.Stonewall, TileTypes.Background, TileTypes.Background} ,
+ {TileTypes.Stonewall, TileTypes.Thorntendrils, TileTypes.Thorntendrils, TileTypes.Thorntendrils, TileTypes.Background, TileTypes.Background, TileTypes.Background} ,
+ {TileTypes.Background, TileTypes.Background, TileTypes.Thorntendrils, TileTypes.Background, TileTypes.Background, TileTypes.DeepC_270, TileTypes.Water} ,
+ {TileTypes.Background, TileTypes.Water, TileTypes.Thorntendrils, TileTypes.Background, TileTypes.DeepC_180, TileTypes.DeepA_90, TileTypes.Background} ,
  {TileTypes.Background, TileTypes.Background, TileTypes.Thorntendrils, TileTypes.Background, TileTypes.Background, TileTypes.DeepC_90, TileTypes.Background} ,
  {TileTypes.Background, TileTypes.Background, TileTypes.Thorntendrils, TileTypes.Thorntendrils, TileTypes.Background, TileTypes.Background, TileTypes.Background} ,
  {TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Thorntendrils, TileTypes.Thorntendrils, TileTypes.Background, TileTypes.Background} ,
@@ -33,8 +33,8 @@ static public TileTypes[,] tilemap = new TileTypes[,] {
  {TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Background, TileTypes.Thorntendrils} 
 };
 
-static public SpawnPoint[] catSpawnPoints = new SpawnPoint[] {new SpawnPoint(new Vector2(1, -6), 1)};
-static public SpawnPoint[] turtleSpawnPoints = new SpawnPoint[] {new SpawnPoint(new Vector2(12, -6), 1)};
-static public SpawnPoint[] birdSpawnPoints = new SpawnPoint[] {new SpawnPoint(new Vector2(8, 0), 1)};
+static public SpawnPoint[] catSpawnPoints = new SpawnPoint[] {new SpawnPoint(new Vector2(1, -6), 2)};
+static public SpawnPoint[] turtleSpawnPoints = new SpawnPoint[] {new SpawnPoint(new Vector2(12, -6), 2)};
+static public SpawnPoint[] birdSpawnPoints = new SpawnPoint[] {new SpawnPoint(new Vector2(8, 0), 3)};
 }
 
